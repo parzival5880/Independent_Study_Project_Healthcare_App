45149c7bee17c6119215bcf4e42cb463bebb2292
Luv Patel <77385999+parzival5880@users.noreply.github.com>
2024-10-08 16:10:08 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Login'; // Make sure these paths are correct based on where your files are located
 import SignUp from './SignUp';
-import './App.css'; // Keep your App.css if you need global styling
+import SensorManager from './SensorManager';
+import './App.css';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/sensormanager" element={<SensorManager />} />
           <Route path="*" element={<Login />} /> {/* Default route */}
         </Routes>
       </div>
